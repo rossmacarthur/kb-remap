d694299f724a9d89891c26ad1f9c9a805fdb8712
Ross MacArthur <ross@macarthur.io>
2021-03-17 21:56:44 +0200

    Add `UserKeyMapping` and `Keyboard` types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Modified {
+    #[serde(rename = "HIDKeyboardModifierMappingSrc")]
+    src: u64,
+    #[serde(rename = "HIDKeyboardModifierMappingDst")]
+    dst: u64,
+}
+
+#[derive(Serialize)]
+struct UserKeyMapping {
+    #[serde(rename = "UserKeyMapping")]
+    modified: Vec<Modified>,
+}
+
+#[derive(Serialize)]
+struct Keyboard {
+    #[serde(rename = "VendorID")]
+    vendor_id: u64,
+    #[serde(rename = "ProductID")]
+    product_id: u64,
+}
+
+fn main() -> Result<()> {
+    todo!()
 }
