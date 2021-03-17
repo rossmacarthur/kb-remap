0e62b0f9f89d5cb24a58674c05ffa642db6ca0c9
Ross MacArthur <ross@macarthur.io>
2021-03-17 22:02:22 +0200

    Lookup keyboard by name

---
+++ src/command.rs
@@ -0,0 +1,39 @@
+use std::process;
+
+use anyhow::{bail, Context, Result};
+
+/// Nicely format an error message for when the subprocess didn't exit
+/// successfully.
+pub fn format_error_msg(cmd: &process::Command, output: process::Output) -> String {
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    let mut msg = format!(
+        "subprocess didn't exit successfully `{:?}` ({})",
+        cmd, output.status
+    );
+    if !stdout.trim().is_empty() {
+        msg.push_str(&format!("\n--- stdout\n{}", stdout));
+    }
+    if !stderr.trim().is_empty() {
+        msg.push_str(&format!("\n--- stderr\n{}", stderr));
+    }
+    msg
+}
+
+pub trait CommandExt {
+    /// Run the command return the standard output as a UTF-8 string.
+    fn output_text(&mut self) -> Result<String>;
+}
+
+impl CommandExt for process::Command {
+    /// Run the command return the standard output as a UTF-8 string.
+    fn output_text(&mut self) -> Result<String> {
+        let output = self
+            .output()
+            .with_context(|| format!("could not execute subprocess: `{:?}`", self))?;
+        if !output.status.success() {
+            bail!(format_error_msg(self, output));
+        }
+        String::from_utf8(output.stdout).context("failed to parse stdout")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+mod command;
+
+use std::collections::HashMap;
+use std::process;
+
 use anyhow::Result;
+use regex_macro::regex;
 use serde::Serialize;
+
+use crate::command::CommandExt;
 
 #[derive(Serialize)]
 struct Modified {
@@ -15,12 +23,33 @@ struct UserKeyMapping {
     modified: Vec<Modified>,
 }
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
 struct Keyboard {
     #[serde(rename = "VendorID")]
     vendor_id: u64,
     #[serde(rename = "ProductID")]
     product_id: u64,
+}
+
+/// Find a keyboard by name.
+fn lookup_keyboard(name: &str) -> Result<Keyboard> {
+    let text = process::Command::new("ioreg")
+        .args(&["-p", "IOUSB", "-x", "-n", name])
+        .output_text()?;
+
+    let map: HashMap<&str, u64> = regex!("\"(idProduct|idVendor)\" = 0x([[:alnum:]]+)")
+        .captures_iter(&text)
+        .map(|captures| {
+            (
+                captures.get(1).unwrap().as_str(),
+                u64::from_str_radix(&captures[2], 16).unwrap(),
+            )
+        })
+        .collect();
+    Ok(Keyboard {
+        vendor_id: map["idVendor"],
+        product_id: map["idProduct"],
+    })
 }
 
 fn main() -> Result<()> {
