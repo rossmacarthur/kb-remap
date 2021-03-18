6b481fba4f11412de5c5510e5a0cde7158b7e1a3
Ross MacArthur <ross@macarthur.io>
2021-03-18 11:25:59 +0200

    Restructure crate into library and binary

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+mod command;
+
+use std::collections::HashMap;
+use std::iter;
+use std::num;
+use std::process;
+use std::str;
+
+use anyhow::Result;
+use itertools::Itertools;
+use regex_macro::regex;
+use serde::Serialize;
+
+use crate::command::CommandExt;
+
+/// A keyboard modification.
+#[derive(Debug, Serialize)]
+pub struct Mod {
+    #[serde(rename = "HIDKeyboardModifierMappingSrc")]
+    src: u64,
+    #[serde(rename = "HIDKeyboardModifierMappingDst")]
+    dst: u64,
+}
+
+/// A list of keyboard modifications.
+#[derive(Default, Serialize)]
+pub struct Mods {
+    #[serde(rename = "UserKeyMapping")]
+    mods: Vec<Mod>,
+}
+
+/// A unique keyboard.
+#[derive(Debug, Serialize)]
+pub struct Keyboard {
+    #[serde(rename = "VendorID")]
+    vendor_id: u64,
+    #[serde(rename = "ProductID")]
+    product_id: u64,
+}
+
+impl Keyboard {
+    /// Find a keyboard by name.
+    pub fn lookup_by_name(name: &str) -> Result<Self> {
+        let text = process::Command::new("ioreg")
+            .args(&["-p", "IOUSB", "-x", "-n", name])
+            .output_text()?;
+
+        let map: HashMap<&str, u64> = regex!("\"(idProduct|idVendor)\" = 0x([[:alnum:]]+)")
+            .captures_iter(&text)
+            .map(|captures| {
+                (
+                    captures.get(1).unwrap().as_str(),
+                    u64::from_str_radix(&captures[2], 16).unwrap(),
+                )
+            })
+            .collect();
+        Ok(Self {
+            vendor_id: map["idVendor"],
+            product_id: map["idProduct"],
+        })
+    }
+
+    /// Apply the modifications to the keyboard.
+    pub fn apply(&self, mods: Mods) -> Result<()> {
+        process::Command::new("hidutil")
+            .arg("property")
+            .arg("--matching")
+            .arg(&serde_json::to_string(self)?)
+            .arg("--set")
+            .arg(&serde_json::to_string(&mods)?)
+            .output_text()?;
+        Ok(())
+    }
+
+    /// Remove all modifications from the keyboard.
+    pub fn reset(&self) -> Result<()> {
+        self.apply(Mods::default())
+    }
+}
+
+impl str::FromStr for Mod {
+    type Err = num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (raw_src, raw_dst) = s.splitn(2, ':').next_tuple().unwrap();
+        let src = u64::from_str_radix(raw_src.trim_start_matches("0x"), 16)?;
+        let dst = u64::from_str_radix(raw_dst.trim_start_matches("0x"), 16)?;
+        Ok(Self { src, dst })
+    }
+}
+
+impl iter::FromIterator<Mod> for Mods {
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = Mod>,
+    {
+        Mods {
+            mods: iter.into_iter().collect(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,93 +5,10 @@
 //! cargo run -- --name "USB Keyboard" --map 0x700000039:0x70000002A
 //! ```
 
-mod command;
-
-use std::collections::HashMap;
-use std::num;
-use std::process;
-use std::str;
+use clap::Clap;
 
 use anyhow::Result;
-use clap::Clap;
-use itertools::Itertools;
-use regex_macro::regex;
-use serde::Serialize;
-
-use crate::command::CommandExt;
-
-#[derive(Debug, Serialize)]
-struct Modified {
-    #[serde(rename = "HIDKeyboardModifierMappingSrc")]
-    src: u64,
-    #[serde(rename = "HIDKeyboardModifierMappingDst")]
-    dst: u64,
-}
-
-#[derive(Default, Serialize)]
-struct UserKeyMapping {
-    #[serde(rename = "UserKeyMapping")]
-    modified: Vec<Modified>,
-}
-
-#[derive(Debug, Serialize)]
-struct Keyboard {
-    #[serde(rename = "VendorID")]
-    vendor_id: u64,
-    #[serde(rename = "ProductID")]
-    product_id: u64,
-}
-
-impl Keyboard {
-    /// Find a keyboard by name.
-    fn lookup(name: &str) -> Result<Self> {
-        let text = process::Command::new("ioreg")
-            .args(&["-p", "IOUSB", "-x", "-n", name])
-            .output_text()?;
-
-        let map: HashMap<&str, u64> = regex!("\"(idProduct|idVendor)\" = 0x([[:alnum:]]+)")
-            .captures_iter(&text)
-            .map(|captures| {
-                (
-                    captures.get(1).unwrap().as_str(),
-                    u64::from_str_radix(&captures[2], 16).unwrap(),
-                )
-            })
-            .collect();
-        Ok(Self {
-            vendor_id: map["idVendor"],
-            product_id: map["idProduct"],
-        })
-    }
-
-    /// Apply the user key mapping to the keyboard.
-    fn apply(&self, mapping: UserKeyMapping) -> Result<()> {
-        process::Command::new("hidutil")
-            .arg("property")
-            .arg("--matching")
-            .arg(&serde_json::to_string(self)?)
-            .arg("--set")
-            .arg(&serde_json::to_string(&mapping)?)
-            .output_text()?;
-        Ok(())
-    }
-
-    /// Remove all user key mappings from the keyboard.
-    fn reset(&self) -> Result<()> {
-        self.apply(UserKeyMapping::default())
-    }
-}
-
-impl str::FromStr for Modified {
-    type Err = num::ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (raw_src, raw_dst) = s.splitn(2, ':').next_tuple().unwrap();
-        let src = u64::from_str_radix(raw_src.trim_start_matches("0x"), 16)?;
-        let dst = u64::from_str_radix(raw_dst.trim_start_matches("0x"), 16)?;
-        Ok(Self { src, dst })
-    }
-}
+use kb_remap::{Keyboard, Mod};
 
 #[derive(Debug, Clap)]
 struct Opt {
@@ -105,16 +22,16 @@ struct Opt {
 
     /// A map of source key to destination key.
     #[clap(short, long, value_name = "SRC:DST")]
-    map: Vec<Modified>,
+    map: Vec<Mod>,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::parse();
-    let keyboard = Keyboard::lookup(&opt.name)?;
+    let keyboard = Keyboard::lookup_by_name(&opt.name)?;
     if opt.reset {
         keyboard.reset()?;
     } else {
-        keyboard.apply(UserKeyMapping { modified: opt.map })?;
+        keyboard.apply(opt.map.into_iter().collect())?;
     }
     Ok(())
 }
